c23009b01b1a06520bca7c2b870910d5dd5a43fb
Your Name <emall@example.com>
2020-03-05 20:12:05 +0800

    修改报告的路径，改为固定的路径。

--- run_suite.py
+++ run_suite.py
@@ -10,7 +10,9 @@ from script.test_params import TestLogin
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestLogin))
-report_path = BASE_DIR+"/report/ihrt{}.html".format(time.strftime('%Y%m%d %H%M%S'))
+# report_path = BASE_DIR+"/report/ihrt{}.html".format(time.strftime('%Y%m%d %H%M%S'))
+#如果要在jenkins当中引入这个报告，那么需要把时间戳删掉
+report_path = BASE_DIR+"/report/ihrt.html"
 with open(report_path,mode='wb') as f:
     runner = HTMLTestRunner(f,verbosity=2,title="ihtl登录接口测试",description="生成报告，更美观")
     runner.run(suite)
